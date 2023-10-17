1644f681608592baf4d5e38af6dbc6764769cec8
zhaojinxin10242 <zhaojinxin10242@ipalfish.com>
2023-10-18 00:45:20 +0800

    Site updated: 2023-10-18 00:45:19

---
+++ format.sh
@@ -0,0 +1,10 @@
+# prettier 帮助文档：https://prettier.io/docs/en/install.html
+
+#检查文件是否已格式化
+#npx prettier . --check
+
+# 格式化所有内容
+#npx prettier . --write
+# 格式化 markdown 文件
+#npx prettier . --write ./**/*.md
+npx prettier . --write --pattern "_posts/**/*.md"
